20d7afb1e7d3c5d673e52b697e13c7f1c93ead53
Tptogiar <2528891112@qq.com>
2022-02-01 12:26:54 +0800

    RestController

--- src/main/java/com/controller/DepartmentController.java
+++ src/main/java/com/controller/DepartmentController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -21,17 +22,16 @@ import java.util.List;
  * @author Tptogiar
  * @since 2022-01-28
  */
-@Controller
+
 @RequestMapping("/department")
+@RestController
 public class DepartmentController {
 
 
     @Autowired
     DepartmentService departmentService;
 
-
     @GetMapping("/depts")
-    @ResponseBody
     public Msg getDepts(){
         List<Department> departments=departmentService.getDepts();
         Msg msg = Msg.success().addData("depts", departments);
